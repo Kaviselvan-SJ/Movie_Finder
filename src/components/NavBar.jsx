37568b74c76d1f8b3c7b1b3903ce224b4039af1d
Kaviselvan-SJ <sjkaviselvan07@gmail.com>
2025-06-17 23:11:12 +0530

    first commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ function NavBar() {
         </div>
         <div className="navbar-links">
             <Link to="/" className="nav-link">Home</Link>
-            <Link to="/favourites" className="nav-link">Favorites</Link>
+            <Link to="/favourites" className="nav-link">Favourites</Link>
         </div>
     </nav>
 }
